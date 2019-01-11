68dd02236fa395b69de331cd2f802a197614e24b
Durva Kapadne <durva@ResNet-41-73.resnet.ucsb.edu>
2019-01-10 18:59:19 -0800

    added HTML and CSS to homepage

--- src/main/java/com/kubernetes/konekt/controller/HomeController.java
+++ src/main/java/com/kubernetes/konekt/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.kubernetes.konekt.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
+	
 	public String home(Model model) {
 		return "home";
 	}
