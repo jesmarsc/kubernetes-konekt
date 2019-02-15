ad3432848680596fb22c2db8d59627f504e776f3
Jesmar Castillo <jesmar@ucsb.edu>
2019-02-15 00:25:31 -0800

    Added Networking metrics functionality

--- src/main/java/com/kubernetes/konekt/metric/Prometheus.java
+++ src/main/java/com/kubernetes/konekt/metric/Prometheus.java
@@ -23,7 +23,7 @@ public class Prometheus {
         return node.get("data").get("result").get(0).get("value").get(1).asDouble();
     }
     
-    public static Double getMemUsage(String instanceUrl) throws IOException {
+    public double getMemUsage(String instanceUrl) throws IOException {
         RestTemplate restTemplate = new RestTemplate();
         // {instance="URL"}
         String params = "%7Binstance%3D%22"+ instanceUrl + "%22%7D";
@@ -32,16 +32,11 @@ public class Prometheus {
         // If a URI is provided, Rest Template will not encode automatically.
         // URI builders do no encode plus signs properly either, must manually provide encoded URI.
         UriComponents uriComponents = UriComponentsBuilder.fromUriString(
-                "http://35.247.41.79:9090/api/v1/query?"
-                + "query=1-sum(node_memory_MemFree_bytes"
-                + params
-                + "%2Bnode_memory_Cached_bytes"
-                + params
-                + "%2Bnode_memory_Buffers_bytes"
-                + params
-                + ")/sum(node_memory_MemTotal_bytes"
-                + params
-                + ")")
+                "http://35.247.41.79:9090/api/v1/query?qeury="
+                + "1-sum(node_memory_MemFree_bytes" + params
+                + "%2Bnode_memory_Cached_bytes" + params
+                + "%2Bnode_memory_Buffers_bytes" + params
+                + ")/sum(node_memory_MemTotal_bytes" + params + ")")
                 .build(true);
         URI uri = uriComponents.toUri();
         String query = restTemplate.getForObject(uri, String.class);
@@ -49,12 +44,45 @@ public class Prometheus {
         return node.get("data").get("result").get(0).get("value").get(1).asDouble();
     }
     
-    public static Double getNetUsage(String instanceUrl) throws IOException {
-        
-        return null;
+    public double getNetInputUsage(String instanceUrl) throws IOException {
+        RestTemplate restTemplate = new RestTemplate();
+        String params = "{instance=\""+ instanceUrl + "\",device=\"eth0\"}";
+        String query = restTemplate.getForObject(
+                "http://35.247.41.79:9090/api/v1/query?query="
+                + "sum(irate(node_network_receive_bytes_total{instance}[2m]))", 
+                String.class, params);
+        JsonNode node = new ObjectMapper().readTree(query);
+        return node.get("data").get("result").get(0).get("value").get(1).asDouble();
     }
     
-    public static void main(String[] args) throws IOException {
-        System.out.println(getMemUsage("35.233.197.146:9090"));
+    public double getNetOutputUsage(String instanceUrl) throws IOException {
+        RestTemplate restTemplate = new RestTemplate();
+        String params = "{instance=\""+ instanceUrl + "\",device=\"eth0\"}";
+        String query = restTemplate.getForObject(
+                "http://35.247.41.79:9090/api/v1/query?query="
+                + "sum(irate(node_network_transmit_bytes_total{instance}[2m]))", 
+                String.class, params);
+        JsonNode node = new ObjectMapper().readTree(query);
+        return node.get("data").get("result").get(0).get("value").get(1).asDouble();
     }
+    
+    public double getNetSaturation(String instanceUrl) throws IOException {
+        RestTemplate restTemplate = new RestTemplate();
+        // {instance="URL",device="eth0"}
+        String params = "%7Binstance%3D%22"+ instanceUrl + "%22,device%3D%22eth0%22%7D";
+        // Plus signs not properly encoded, must use builder without encoding.
+        // Rest template will automatically encode, cannot use.
+        // If a URI is provided, Rest Template will not encode automatically.
+        // URI builders do no encode plus signs properly either, must manually provide encoded URI.
+        UriComponents uriComponents = UriComponentsBuilder.fromUriString(
+                "http://35.247.41.79:9090/api/v1/query?query="
+                + "sum(irate(node_network_receive_drop_total" + params + "%5B2m%5D))"
+                + "%2Bsum(irate(node_network_transmit_drop_total" + params + "%5B2m%5D))")
+                .build(true);
+        URI uri = uriComponents.toUri();
+        String query = restTemplate.getForObject(uri, String.class);
+        JsonNode node = new ObjectMapper().readTree(query);
+        return node.get("data").get("result").get(0).get("value").get(1).asDouble();
+    } 
+
 }
